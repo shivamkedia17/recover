3e6a6df5e2c12d95dc547d1b83e617cf485dee1d
shivamkedia17 <shivamkedia17@users.noreply.github.com>
2020-05-29 10:39:37 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
+
+//For CS50x from harvard.edu 
+//Hello
+//Author - shivamkedia17 (GitHub)
+
+/** This program says hello to the person whose name is entered.*/ 
+
 int main(void)
 {
+    //Taking user input
     string name = get_string("Enter your name\n");
-    printf("hello, %s\n",name);
+    
+    //Saying hello to the name input
+    printf("hello, %s\n", name);
 }
