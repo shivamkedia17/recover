f8b9db779ad952cf51c41a29be220d2ed4ec2804
shivamkedia17 <shivamkedia17@users.noreply.github.com>
2020-05-29 09:03:53 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+//Mario (More Comfortable)
+
+int main(void)
+{
+    //Taking user input
+    int n; 
+    do
+    {
+        n = get_int("Height: ");
+    }while(n<1 || n>8);
+    
+    //Displaying the blocks
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-i; j>=0; j--)
+        {
+            printf(" "); 
+        }
+        for(int j=0;j<=i;j++)
+        {
+            printf("#");
+        }
+        
+        printf("  ");
+        
+        for(int j=0;j<=i;j++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+}
